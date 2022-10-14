ad5957a7200b57848a113ce27102990e64a2db0f
aishwarya-phatak <ashp17star@gmail.com>
2022-10-14 15:22:27 +0530

    Added print message to View Controller

--- FirstApplication/ViewController.swift
+++ FirstApplication/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("Welcome to iOS September Batch!")
         bindDataToViews()
     }
     
